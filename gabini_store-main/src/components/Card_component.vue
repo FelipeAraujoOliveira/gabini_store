c70bdb63938fcc0cea09bd6cb2f48611357636b3
Enrico Sotelo <127902763+enricosotelo@users.noreply.github.com>
2024-09-18 15:57:59 -0300

    Componentes feitos, HomeView atualizada, imagens upadas na nuvem.
    Feito todos os componentes existentes, commitado na main branch após análise. Imagens upadas na nuvem e não mais em uma pasta dentro do projeto.
    Link das imagens:  
    ```
    https://ibb.co/60cQ4sr
    https://ibb.co/sHVW2Gp
    https://ibb.co/t2Q7rTp
    https://ibb.co/DMTWxNC
    https://ibb.co/Xb2bFfd
    https://ibb.co/YkhnX5m
    https://ibb.co/9w02v0V
    https://ibb.co/9Y51SyL
    https://ibb.co/J5Jjd0v
    https://ibb.co/6gFTcj7
    https://ibb.co/D56NFTQ  
    ```

---
+++ gabini_store-main/src/components/Card_component.vue
@@ -0,0 +1,131 @@
+<template>
+    
+    <section class="container-box">
+    <div class="container">
+      <div class="hp-card">
+        <div class="atribute">
+          <div class="atribute-box">
+            <div class="atribute-ball">
+              <i class="bi bi-box"></i>
+            </div>
+            <div class="atribute-info">
+              <span>Item wheight</span>
+              <p><b>2.6 Pounds</b></p>
+            </div>
+          </div>
+        </div>
+        <h1>Find the <br> perfect gifts</h1>
+      </div>
+      <div class="hp-card2">
+        <div class="atribute">
+          <div class="atribute-box">
+            <div class="atribute-ball">
+              <i class="bi bi-box"></i>
+            </div>
+            <div class="atribute-info">
+              <span>Item wheight</span>
+              <p><b>2.6 Pounds</b></p>
+            </div>
+          </div>
+        </div>
+        <h1>Find the <br> perfect gifts</h1>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+
+<script>
+</script>
+
+
+<style>
+
+
+.hp-card {
+  border-radius: 30px;
+  width: 27.6vw; /* 530px */
+  height: 27.6vw; /* 530px */
+  background-image: url("https://i.ibb.co/pyLhx86/fone.png");
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 1.8rem;
+
+  & h1 {
+      color: white;
+      font-size: 4.1rem;
+  }
+}
+
+.hp-card:hover {
+  cursor: pointer;
+  background-image: linear-gradient(to left, #0000005a, #0000005a), url("https://i.ibb.co/pyLhx86/fone.png");
+}
+
+.hp-card2 {
+  background-image: url("https://i.ibb.co/pj6q3Xb/5225ea06c091a3f8941951fba23f3d10.png");
+  border-radius: 30px;
+  width: 27.6vw; /* 530px */
+  height: 27.6vw; /* 530px */
+  background-position: 50%;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 30px;
+
+  & h1 {
+      color: white;
+      font-size: 55px;
+  }
+}
+
+.hp-card2:hover {
+  cursor: pointer;
+  background-image: linear-gradient(to left, #0000005a, #0000005a), url("https://i.ibb.co/pj6q3Xb/5225ea06c091a3f8941951fba23f3d10.png");
+}
+
+.atribute {
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  width: 240px;
+  height: 78px;
+  border-radius: 80px;
+  background: rgba(49, 49, 49, 0.572);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(6.7px);
+  -webkit-backdrop-filter: blur(6.7px);
+}
+
+.atribute-box {
+  align-items: center;
+  display: flex;
+  width: 90%;
+  height: 90%;
+}
+
+
+.atribute-ball {
+  width: 54px;
+  height: 54px;
+  border-radius: 50%;
+  justify-content: center;
+  display: flex;
+  font-size: 24px;
+  align-items: center;
+  background-color: #ffffff;
+  color: #000000;
+}
+
+.atribute-info {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  margin-left: 20px;
+  color: #ffffff;
+}
+</style>
---
+++ gabini_store-main/src/components/Showcase_component.vue
@@ -0,0 +1,214 @@
+<template>
+    <section class="container-box2">
+        <div class="container">
+          <div class="phones-box">
+            <h1>new products</h1>
+            <p>Choose the headset that best suits your needs. We offer top-quality options with <br> a variety of features
+              to enhance your audio experience</p>
+            <div class="card-box">
+              <div class="card">
+                <div class="phone-efect">
+                  <img class="phone3" src="https://i.ibb.co/qg7tQ3Y/g433-gallery-black-1-1-1.png" alt="">
+                  <img src="https://i.ibb.co/09v2gkM/g433-gallery-black-1-1.png" alt="" class="phone4">
+                </div>
+                <p>Gabini® K-29 Premium Headset <br> with Multi-Device Connectivity <br> and Noise-Canceling <br>
+                  Technology, Black</p>
+                <div class="prices">
+                  <p>$</p>
+                  <h1>94.99</h1>
+                  <span>$130.00</span>
+                </div>
+                <div class="cart-btn">
+                  <a href="">add to cart</a>
+                  <div class="cart-icon-box">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="phone-efect">
+                  <img class="phone3" src="https://i.ibb.co/qg7tQ3Y/g433-gallery-black-1-1-1.png" alt="">
+                  <img src="https://i.ibb.co/09v2gkM/g433-gallery-black-1-1.png" alt="" class="phone4">
+                </div>
+                <p>Gabini® K-29 Premium Headset <br> with Multi-Device Connectivity <br> and Noise-Canceling <br>
+                  Technology, Black</p>
+                <div class="prices">
+                  <p>$</p>
+                  <h1>94.99</h1>
+                  <span>$130.00</span>
+                </div>
+                <div class="cart-btn">
+                  <a href="">add to cart</a>
+                  <div class="cart-icon-box">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="phone-efect">
+                  <img class="phone3" src="https://i.ibb.co/qg7tQ3Y/g433-gallery-black-1-1-1.png" alt="">
+                  <img src="https://i.ibb.co/09v2gkM/g433-gallery-black-1-1.png" alt="" class="phone4">
+                </div>
+                <p>Gabini® K-29 Premium Headset <br> with Multi-Device Connectivity <br> and Noise-Canceling <br>
+                  Technology, Black</p>
+                <div class="prices">
+                  <p>$</p>
+                  <h1>94.99</h1>
+                  <span>$130.00</span>
+                </div>
+                <div class="cart-btn">
+                  <a href="">add to cart</a>
+                  <div class="cart-icon-box">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+</template>
+
+
+<script></script>
+
+
+<style>
+
+.phones-box {
+    height: auto;
+    padding: 80px 0px 80px 0px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #F3F5F9;
+    border-radius: 30px;
+    text-align: center;
+    gap: 2.08vw; /* 40px */
+
+    & h1 {
+        font-size: 2.29vw; /* 44px */
+        text-transform: uppercase;
+    }
+
+    & p {
+        font-weight: 200;
+    }
+}
+
+.card-box {
+    width: 100%;
+    align-items: center;
+    justify-content: space-around;
+    display: flex;
+}
+
+.card {
+    gap: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    & p {
+        font-weight: 200;
+    }
+
+    & img {
+        width: 244px;
+        height: 267px;
+    }
+
+    & .prices {
+        justify-content: center;
+        position: relative;
+        display: flex;
+        gap: 3px;
+
+        & {
+            span {
+                margin-top: 5px;
+                color: red;
+                text-decoration: line-through;
+
+                font-weight: bold;
+            }
+        }
+
+        & h1 {
+            font-size: 40px;
+        }
+
+        & p {
+            font-weight: bold;
+            margin-top: 5px;
+        }
+    }
+}
+
+a {
+    color: #000;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+.phone-efect {
+    overflow: hidden;
+
+    width: 244px;
+    height: 267px;
+
+    & .phone3:hover {
+        display: none;
+    }
+
+    & .phone4 {
+        display: none;
+    }
+
+
+}
+
+.phone-efect:hover {
+    & .phone4 {
+        display: block;
+    }
+}
+
+
+
+.cart-btn {
+    width: 7.29vw; /* 140px */
+    height: 2.6vw; /* 50px */
+    padding-left: 20px;
+    padding-right: 10px;
+    justify-content: space-between;
+    display: flex;
+    align-items: center;
+    border-radius: 30px;
+    background-color: #fff;
+    font-size: 12px;
+    border: 1px solid black;
+}
+
+.cart-icon-box {
+    width: 1.56vw; /* 30px */
+    height: 1.56vw; /* 30px */
+    background-color: #000;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    font-size: 12px;
+
+    & i {
+        margin: 0 !important;
+    }
+}
+
+</style>
